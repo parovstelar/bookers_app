030c13a92c3ae3c47bd065a8cfe311d35ef6371a
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-11-10 10:24:36 +0000

    [finish] 3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'homes#top'
+# get '/top' => 'homes#top'
+  resources :books
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
